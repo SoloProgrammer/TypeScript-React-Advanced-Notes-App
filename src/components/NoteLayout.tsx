f6dfb6187b5a6d98e0410fca16b7897aef081837
SoloProgrammer <prathamshinde987@gmail.com>
2024-03-03 10:13:07 +0530

    Created Docker file and contenerize the application and publish the container to docker hub!

--- src/components/NoteLayout.tsx
+++ src/components/NoteLayout.tsx
@@ -1,4 +1,9 @@
-import { Navigate, Outlet, useOutletContext, useParams } from "react-router-dom";
+import {
+  Navigate,
+  Outlet,
+  useOutletContext,
+  useParams,
+} from "react-router-dom";
 import { Note } from "../types/Notestypes";
 
 type NoteLayoutProps = {
@@ -9,9 +14,7 @@ const NoteLayout = ({ notes }: NoteLayoutProps) => {
   const { id } = useParams();
   const note = notes.find((note) => note.id === id);
 
-  if(!note) return <Navigate to={'/'} replace/>
-
-  return <Outlet context={note} />;
+  return !note ? <Navigate to={"/"} replace /> : <Outlet context={note} />;
 };
 
 export default NoteLayout;
