bb8411155a718b7f7271b3cd9337021f6ea083fa
SoloProgrammer <prathamshinde987@gmail.com>
2024-03-27 13:58:52 +0530

    addded new Note types

--- src/types/Notestypes.ts
+++ src/types/Notestypes.ts
@@ -8,6 +8,7 @@ export type RawNote = {
 
 export type RawNoteData = {
   title: string;
+  isPinned: false | true;
   markdown: string;
   tagIds: string[];
 };
@@ -15,6 +16,7 @@ export type RawNoteData = {
 export type NoteData = {
   title: string;
   markdown: string;
+  isPinned: false | true;
   tags: Tag[];
 };
 
@@ -24,3 +26,33 @@ export type Tag = {
 };
 
 export type SimplifiedNote = Omit<Note, "markdown">;
+
+export type ViewNoteModalProps = {
+  selectedNote: Note | null;
+  handleOffsetClick: () => void;
+  onPinNote: (id: string) => void;
+};
+export type NoteCardProps = {
+  note: SimplifiedNote;
+  onPinNote: (id: string) => void;
+};
+
+export type EditTagsModalProps = {
+  show: boolean;
+  handleCloseModal: () => void;
+  availableTags: Tag[];
+  deleteTag: (id: string) => void;
+  updateTag: (data: Tag[]) => void;
+};
+
+export type EditNoteProps = {
+  onUpdateNote: (id: string, data: NoteData) => void;
+  onAddTag: (data: Tag) => void;
+  availableTags: Tag[];
+};
+
+export type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (data: Tag) => void;
+  availableTags: Tag[];
+} & Partial<NoteData>;
